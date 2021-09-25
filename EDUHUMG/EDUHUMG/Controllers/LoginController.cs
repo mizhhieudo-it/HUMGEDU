43bc470479b31eb7fd37bfe269930e5c232c3cc9
Tran Minh Hieu <hieuminhtran2905gmail.com>
2021-09-25 23:29:04 +0700

    file 25/9

---
+++ EDUHUMG/EDUHUMG/Controllers/LoginController.cs
@@ -0,0 +1,106 @@
+﻿using EDUHUMG.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static EDUHUMG.Common.ClassCommon;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EDUHUMG.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/<LoginController>
+        /*  [HttpPost]
+          public IEnumerable<Taikhoan> Post()
+          {
+              //using ( var context = new HUMGEDUContext())
+              // {
+              //     return context.Taikhoans.ToList();
+
+              // }
+
+              HUMGEDUContext context = new HUMGEDUContext();
+              return context.Taikhoans.ToList();
+          }*/
+        [HttpPost("thongtinuser"), FormatFilter]
+        public IEnumerable<Taikhoan> Thongtin([FromForm] layuser parameter)
+        {
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Taikhoan> taikhoans = context.Taikhoans.ToList();
+            return taikhoans;
+        }
+        // POST api/<LoginController>
+        [HttpPost("layuser"), FormatFilter]
+        public IEnumerable<Taikhoan> Post([FromForm] layuser parameter)
+        {
+            HUMGEDUContext context = new HUMGEDUContext();
+
+            List<Taikhoan> ketqua = new List<Taikhoan>();
+            List<Taikhoan> taikhoans = context.Taikhoans.ToList();
+
+            //C1
+            int checkLoginSuccess = taikhoans.Where(tk => tk.Username == parameter.user && tk.Password == parameter.pass).Count();
+
+            /* //C2
+             int checkLoginSuccess2 = 0;
+             foreach (Taikhoan tk in taikhoans)
+             {
+                 if (tk.Username == parameter.user && tk.Password == parameter.pass)
+                 {
+                     checkLoginSuccess2++;
+                 }
+             }*/
+
+            if (checkLoginSuccess > 0)
+            {
+                ketqua = taikhoans.Where(tk => tk.Iduser == parameter.idcanlay).ToList();
+
+            }
+            else
+            {
+                // không có gì
+            }
+
+            return ketqua;
+
+
+        }
+
+        [HttpPost("login"), FormatFilter]
+        public Status Posst([FromForm] layuser parameter)
+        {
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Taikhoan> taikhoans = context.Taikhoans.ToList();
+
+            Status status = new Status();
+
+
+
+            int checkLoginSuccess = taikhoans.Where(tk => tk.Username == parameter.user && tk.Password == parameter.pass).Count();
+
+            if (checkLoginSuccess > 0)
+            {
+                status.status = true;
+                status.message = "success";
+                status.code = 200;
+               // status.data = taikhoans;
+
+
+            }
+            else
+            {
+                status.status = false;
+                status.message = "fail";
+                status.code = 401;
+            }
+
+            return status;
+        }
+    }
+}
---
+++ EDUHUMG/EDUHUMG/Controllers/ThongbaoController.cs
@@ -0,0 +1,94 @@
+﻿using EDUHUMG.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static EDUHUMG.Common.ClassCommon;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EDUHUMG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThongbaoController : ControllerBase
+    {
+        // GET: api/<ThongbaoController>
+        [HttpGet]
+        public IEnumerable<Thongbao> Get()
+        {
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Thongbao> thongbaos = new List<Thongbao>();
+            return thongbaos;
+        }
+        [HttpGet("{thongbao}")]
+        public IEnumerable<Thongbao> Gett()
+        {
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Thongbao> thongbaos = context.Thongbaos.ToList();
+            return thongbaos;
+        }
+        [HttpGet("{laychitiet}")]
+        public IEnumerable<Thongbao> Get1(int id)
+        {
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Thongbao> thongbaos = context.Thongbaos.ToList();
+            IEnumerable<Thongbao> ketqua = from d in thongbaos
+                                           where d.Idthongbao == id
+                                           select d;
+            return ketqua;
+        }
+        [HttpGet("{xoathongbao}")]
+        public Status XoaThongBao(int Id)
+        {
+            Status status = new Status();
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Thongbao> thongbaos = context.Thongbaos.ToList();
+            Thongbao ketqua = context.Thongbaos.Single(x => x.Idthongbao == Id);
+            if (ketqua == null)
+            {
+                context.Thongbaos.Remove(ketqua);
+                context.SaveChanges();
+                status.status = true;
+                status.message = "success";
+                status.code = 200;
+            }
+            else
+            {
+                status.status = true;
+                status.message = "success";
+                status.code = 200;
+            }
+
+
+
+            return status;
+        }
+
+        // GET api/<ThongbaoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ThongbaoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ThongbaoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ThongbaoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
