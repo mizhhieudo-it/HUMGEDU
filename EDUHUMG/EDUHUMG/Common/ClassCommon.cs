9f771c0f31e6532445caf4eb72698092e4575b6c
Tran Minh Hieu <hieuminhtran2905gmail.com>
2021-09-27 17:00:08 +0700

    file 27/9

--- EDUHUMG/EDUHUMG/Common/ClassCommon.cs
+++ EDUHUMG/EDUHUMG/Common/ClassCommon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EDUHUMG.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,25 @@ namespace EDUHUMG.Common
 {
     public class ClassCommon
     {
+        public class layuser
+        {
+            public string user { get; set; }
+            public string pass { get; set; }
+            public int idcanlay { get; set; }
+        }
+
+        public class Status
+        {
+            public bool status { get; set; }
+            public string message { get; set; }
+
+            public int code { get; set; }
+            public Danhmuc data { get; set; }
+        }
+
+        public class DanhmucChicoten
+        {
+            public string tendanhmuc { get; set; }
+        }
     }
 }
--- EDUHUMG/EDUHUMG/Controllers/DanhmucController.cs
+++ EDUHUMG/EDUHUMG/Controllers/DanhmucController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EDUHUMG.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static EDUHUMG.Common.ClassCommon;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,92 @@ namespace EDUHUMG.Controllers
     [ApiController]
     public class DanhmucController : ControllerBase
     {
-        // GET: api/<DanhmucController>
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<DanhmucChicoten> Get()
         {
-            return new string[] { "value1", "value2" };
+
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Danhmuc> danhmucs = context.Danhmucs.ToList();
+
+            IEnumerable<DanhmucChicoten> ketqua = (from d in danhmucs
+                                                   select new DanhmucChicoten()
+                                                   { tendanhmuc = d.Tendanhmuc });
+            return ketqua;
         }
 
-        // GET api/<DanhmucController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("timdm/{id}")]
+        public IEnumerable<Danhmuc> Timdanhmuc(int id)
         {
-            return "value";
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Danhmuc> danhmucs = context.Danhmucs.ToList();
+            IEnumerable<Danhmuc> ketqua = from d in danhmucs
+                                          where d.Iddanhmuc == id
+                                          select d; ;
+            return ketqua;
+        }
+
+        [HttpGet("xoadm/{id}")]
+        public Status Xoadm(int id)
+        {
+            Status status = new Status();
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Danhmuc> danhmucs = context.Danhmucs.ToList();
+            Danhmuc ketqua = context.Danhmucs.Single(x => x.Iddanhmuc == id);
+            if (ketqua != null)
+            {
+                context.Danhmucs.Remove(ketqua);
+                context.SaveChanges();
+                status.status = true;
+                status.message = "success";
+                status.code = 200;
+
+
+            }
+            else
+            {
+                status.status = false;
+                status.message = "fail";
+                status.code = 401;
+            }
+
+            return status;
+
         }
 
         // POST api/<DanhmucController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("themdm")]
+        public Status Post([FromBody] Danhmuc dulieu)
         {
-        }
+            Status status1 = new Status();
+            HUMGEDUContext context = new HUMGEDUContext();
+            List<Danhmuc> danhmucs = context.Danhmucs.ToList();
 
-        // PUT api/<DanhmucController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+            if (dulieu != null)
+            {
+                context.Danhmucs.Add(dulieu);
+                context.SaveChanges();
+                status1.code = 200;
+                status1.status = true;
+                status1.message = "suucces";
+                status1.data = dulieu;
+            }
+            else
+            {
+
+                status1.code = 401;
+                status1.status = false;
+                status1.message = "fail";
+            }
+            return status1;
+        }
+        /*[HttpPost("suadm/{id}")]
+
+        public Status Suadm([FromBody] Danhmuc dulieu)
         {
-        }
 
+        }*/
         // DELETE api/<DanhmucController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+
     }
 }
